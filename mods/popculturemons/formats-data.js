76014adf1cf9c6a449489c69768c965f71b75435
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-08-06 13:43:09 -0700

    Update formats-data.js

--- mods/popculturemons/formats-data.js
+++ mods/popculturemons/formats-data.js
@@ -3,6 +3,14 @@ exports.BattleFormatsData = {
                 inherit: true,
                 tier: 'OU'
         },
+        megamanbomb: {
+                inherit: true,
+                tier: 'OU'
+        },
+        megamanfire: {
+                inherit: true,
+                tier: 'OU'
+        },        
         goku: {
                 inherit: true,
                 tier: 'OU'
